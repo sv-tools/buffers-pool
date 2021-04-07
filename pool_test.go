a07a118fc7010ac51f4c02eb3c91f75d3e9c2ecf
Sergey Vilgelm <sergey.vilgelm@ibm.com>
2021-04-07 16:08:19 -0500

    Add the code of Buffer Pool

---
+++ pool_test.go
@@ -0,0 +1,31 @@
+package bufferspool_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	bufferspool "github.com/sv-tools/buffers-pool"
+)
+
+func TestGlobalPool(t *testing.T) {
+	b1 := bufferspool.Get()
+	b2 := bufferspool.Get()
+	require.NotSame(t, b1, b2)
+
+	b1.WriteString("foo")
+	bufferspool.Put(b1)
+	b2 = bufferspool.Get()
+	require.Zero(t, b2.Len())
+}
+
+func TestCustomPool(t *testing.T) {
+	p := bufferspool.New()
+	b1 := p.Get()
+	b2 := p.Get()
+	require.NotSame(t, b1, b2)
+
+	b1.WriteString("foo")
+	p.Put(b1)
+	b2 = p.Get()
+	require.Zero(t, b2.Len())
+}
